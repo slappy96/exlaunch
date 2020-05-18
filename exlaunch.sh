fc093803de2024501c09e1db81d20af3338e5dc4
slappy96 <jqmunos@gmail.com>
2020-05-18 10:51:35 -0500

    change to rawgithubusercontent

--- exlaunch.sh
+++ exlaunch.sh
@@ -15,7 +15,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 esac done
 
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/slappy96/exvel.git"
-[ -z "$progsfile" ] && progsfile="https://github.com/slappy96/exlaunch/blob/master/progs.csv"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/slappy96/exlaunch/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
