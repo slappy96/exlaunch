f6616894d1b2aa50674112e679ca270864e8ee86
slappy96 <jqmunos@gmail.com>
2021-02-14 15:37:01 -0600

    Edited to 80 LOC-Horizontal +prettied up

--- exlaunch.sh
+++ exlaunch.sh
@@ -6,7 +6,10 @@
 ### OPTIONS AND VARIABLES ###
 
 while getopts ":a:r:b:p:h" o; do case "${o}" in
-	h) printf "Optional arguments for custom use:\\n  -r: Dotfiles repository (local file or url)\\n  -p: Dependencies and programs csv (local file or url)\\n  -a: AUR helper (must have pacman-like syntax)\\n  -h: Show this message\\n" && exit 1 ;;
+	h) printf "Optional arguments for custom use:\\n  -r: Dotfiles repository
+	(local file or url)\\n  -p: Dependencies and programs csv (local file or
+	url)\\n  -a: AUR helper (must have pacman-like syntax)\\n  -h: Show this
+	message\\n" && exit 1 ;;
 	r) dotfilesrepo=${OPTARG} && git ls-remote "$dotfilesrepo" || exit 1 ;;
 	b) repobranch=${OPTARG} ;;
 	p) progsfile=${OPTARG} ;;
@@ -15,49 +18,73 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 esac done
 
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/slappy96/exvel.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/slappy96/exlaunch/master/progs.csv"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/slappy96/
+exlaunch/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
 ### FUNCTIONS ###
 
-	installpkg(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
+installpkg(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
 
 error() { clear; printf "ERROR:\\n%s\\n" "$1" >&2; exit 1;}
 
 welcomemsg() { \
-	dialog --title "Welcome!" --msgbox "Welcome to exitvelocity's install script\\n\\nThis script will automatically install a fully-featured Linux desktop, you will be more efficient - haxor.\\n\\n-Slappy" 10 60
+	dialog --title "Welcome!" --msgbox "Welcome to exitvelocity's install
+	script\\n\\nThis script will automatically install a fully-featured Linux
+	desktop, you will be more efficient - haxor.\\n\\n-Slappy" 10 60
 	}
 
 getuserandpass() { \
 	# Prompts user for new username an password.
-	name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
+	name=$(dialog --inputbox "First, please enter a name for the user account."
+	10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
 	while ! echo "$name" | grep -q "^[a-z_][a-z0-9_-]*$"; do
-		name=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
+	name=$(dialog --no-cancel --inputbox "Username not valid. Give a username
+	beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1
+	1>&2 2>&3 3>&1)
 	done
-	pass1=$(dialog --no-cancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
-	pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	pass1=$(dialog --no-cancel --passwordbox "Enter a password for that user."
+	10 60 3>&1 1>&2 2>&3 3>&1)
+	pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1
+	1>&2 2>&3 3>&1)
 	while ! [ "$pass1" = "$pass2" ]; do
 		unset pass2
-		pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
-		pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
-	done ;}
+		pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\n
+		Enter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
+		pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1
+		1>&2 2>&3 3>&1) done ;}
 
 usercheck() { \
 	! { id -u "$name" >/dev/null 2>&1; } ||
-	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. exvel can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nexvel will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that exvel will change $name's password to the one you just gave." 14 70
+	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No
+	wait..." --yesno "The user \`$name\` already exists on this system. exvel
+	can install for a user already existing, but it will \\Zboverwrite\\Zn any
+	conflicting settings/dotfiles on the user account.\\n\\nexvel will \\Zbnot
+	\\Zn overwrite your user files, documents, videos, etc., so don't worry
+	about that, but only click <CONTINUE> if you don't mind your settings
+	being overwritten.\\n\\nNote also that exvel will change $name's password
+	to the one you just gave." 14 70
 	}
 
 preinstallmsg() { \
-	dialog --title "Let's begin launch sequence!" --yes-label "Kick the tires and light the fires!" --no-label "No, abort launch!" --yesno "The rest of the installation will now be like a good zero G nap, so kick your feet up comrade.\\n\\nIt will take some time, but when done, you can surf the interwebs like a boss.\\n\\nNow just press <Kick the tires and light the fires!!> and the system will begin installation!" 13 70 || { clear; exit 1; }
+	dialog --title "Let's begin launch sequence!" --yes-label "Kick the tires
+	and light the fires!" --no-label "No, abort launch!" --yesno "The rest of
+	the installation will now be like a good zero G nap, so kick your feet up
+	comrade.\\n\\nIt will take some time, but when done, you can surf the
+	interwebs like a boss.\\n\\nNow just press <Kick the tires and light the
+	fires!!> and the system will begin installation!" 13 70 || { clear; exit
+	1; }
 	}
 
 adduserandpass() { \
 	# Adds user `$name` with password $pass1.
 	dialog --infobox "Adding user \"$name\"..." 4 50
 	useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
-	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
-	repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel "$(dirname "$repodir")"
+	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel
+	/home/"$name"
+	repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel
+	"$(dirname "$repodir")"
 	echo "$name:$pass1" | chpasswd
 	unset pass1 pass2 ;}
 
@@ -70,7 +97,8 @@ newperms() { # Set special sudoers settings for install (or after).
 	sed -i "/#INTERLOPER/d" /etc/sudoers
 	echo "$* #INTERLOPER" >> /etc/sudoers ;}
 
-manualinstall() { # Installs $1 manually if not installed. Used only for AUR helper here.
+manualinstall() { # Installs $1 manually if not installed. Used only for AUR
+	# helper here.
 	[ -f "/usr/bin/$1" ] || (
 	dialog --infobox "Installing \"$1\", an AUR helper..." 4 50
 	cd /tmp || exit 1
@@ -82,39 +110,46 @@ manualinstall() { # Installs $1 manually if not installed. Used only for AUR hel
 	cd /tmp || return 1) ;}
 
 maininstall() { # Installs all needed programs from main repo.
-	dialog --title "exvel Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
+	dialog --title "exvel Installation" --infobox "Installing \`$1\` ($n of
+	$total). $1 $2" 5 70
 	installpkg "$1"
 	}
 
 gitmakeinstall() {
 	progname="$(basename "$1" .git)"
 	dir="$repodir/$progname"
-	dialog --title "exvel Installation" --infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
-	sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return 1 ; sudo -u "$name" git pull --force origin master;}
+	dialog --title "exvel Installation" --infobox "Installing \`$progname\`
+	($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
+	sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 ||
+	{ cd "$dir" || return 1 ; sudo -u "$name" git pull --force origin master;}
 	cd "$dir" || exit 1
 	make >/dev/null 2>&1
 	make install >/dev/null 2>&1
 	cd /tmp || return 1 ;}
 
 aurinstall() { \
-	dialog --title "exvel Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
+	dialog --title "exvel Installation" --infobox "Installing \`$1\` ($n of
+	$total) from the AUR. $1 $2" 5 70
 	echo "$aurinstalled" | grep -q "^$1$" && return 1
 	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
 	}
 
 pipinstall() { \
-	dialog --title "exvel Installation" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 5 70
+	dialog --title "exvel Installation" --infobox "Installing the Python
+	package \`$1\` ($n of $total). $1 $2" 5 70
 	[ -x "$(command -v "pip")" ] || installpkg python-pip >/dev/null 2>&1
 	yes | pip install "$1"
 	}
 
 installationloop() { \
-	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
+	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls
+	"$progsfile" | sed '/^#/d' > /tmp/progs.csv
 	total=$(wc -l < /tmp/progs.csv)
 	aurinstalled=$(pacman -Qqm)
 	while IFS=, read -r tag program comment; do
 		n=$((n+1))
-		echo "$comment" | grep -q "^\".*\"$" && comment="$(echo "$comment" | sed "s/\(^\"\|\"$\)//g")"
+		echo "$comment" | grep -q "^\".*\"$" && comment="$(echo "$comment"
+		| sed "s/\(^\"\|\"$\)//g")"
 		case "$tag" in
 			"A") aurinstall "$program" "$comment" ;;
 			"G") gitmakeinstall "$program" "$comment" ;;
@@ -123,23 +158,31 @@ installationloop() { \
 		esac
 	done < /tmp/progs.csv ;}
 
-putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
+putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only
+	overwriting conflicts
 	dialog --infobox "Downloading and installing config files..." 4 60
 	[ -z "$3" ] && branch="master" || branch="$repobranch"
 	dir=$(mktemp -d)
 	[ ! -d "$2" ] && mkdir -p "$2"
 	chown -R "$name":wheel "$dir" "$2"
-	sudo -u "$name" git clone --recursive -b "$branch" --depth 1 "$1" "$dir" >/dev/null 2>&1
+	sudo -u "$name" git clone --recursive -b "$branch" --depth 1 "$1"
+	"$dir" >/dev/null 2>&1
 	sudo -u "$name" cp -rfT "$dir" "$2"
 	}
 
-systembeepoff() { dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
+systembeepoff() { dialog --infobox "Getting rid of that retarded error
+	beep sound..." 10 50
 	rmmod pcspkr
 	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
 
 finalize(){ \
 	dialog --infobox "Preparing welcome message..." 4 50
-	dialog --title "All done!" --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t exvel" 12 80
+	dialog --title "All done!" --msgbox "Congrats! Provided there were
+	no hidden errors, the script completed successfully and all the
+	programs and configuration files should be in place.\\n\\nTo run
+	the new graphical environment, log out and log back in as your new
+	user, then run the command \"startx\" to start the graphical
+	environment (it will start automatically in tty1).\\n\\n.t exvel" 12 80
 	}
 
 ### THE ACTUAL SCRIPT ###
@@ -147,7 +190,8 @@ finalize(){ \
 ### This is how everything happens in an intuitive format and order.
 
 # Check if user is root on Arch distro. Install dialog.
-pacman --noconfirm --needed -Sy dialog || error "Are you sure you're running this as the root user and have an internet connection?"
+pacman --noconfirm --needed -Sy dialog || error "Are you sure you're running
+this as the root user and have an internet connection?"
 
 # Welcome user and pick dotfiles.
 welcomemsg || error "User exited."
@@ -165,15 +209,17 @@ preinstallmsg || error "User exited."
 
 
 # Refresh Arch keyrings.
-refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
+refreshkeys || error "Error automatically refreshing Arch keyring. Consider
+doing so manually."
 
 for x in curl base-devel git ntp zsh; do
-	dialog --title "Exvel Installation" --infobox "Installing \'$x\' which is required to install cond configure other programs." 5 70
+	dialog --title "Exvel Installation" --infobox "Installing \'$x\' which
+	is required to install cond configure other programs." 5 70
 	installpkg "$x"
 done
 
 dialog --title "exvel Installation" --infobox "Synchronizing system time to
-	ensure successful and secure installation of software..." 4 70
+ensure successful and secure installation of software..." 4 70
 ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
 
 adduserandpass || error "Error adding username and/or password."
@@ -182,11 +228,13 @@ adduserandpass || error "Error adding username and/or password."
 
 # Allow user to run sudo without password. Since AUR programs must be installed
 # in a fakeroot environment, this is required for all builds with AUR.
-	newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
+newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
 
-# Make pacman and yay colorful and adds eye candy on the progress bar because why not.
+# Make pacman and yay colorful and adds eye candy on the progress bar because
+# why not.
 grep -q "^Color" /etc/pacman.conf || sed -i "s/^#Color$/Color/" /etc/pacman.conf
-grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
+grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy"
+/etc/pacman.conf
 
 # Use all cores for compilation.
 sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
@@ -200,7 +248,8 @@ manualinstall $aurhelper || error "Failed to install AUR helper."
 # and all build dependencies are installed.
 installationloop
 
-dialog --title "exvel Installation" --infobox "Finally, installing \`libxft-bgra\` to enable color emoji in suckless software without crashes." 5 70
+dialog --title "exvel Installation" --infobox "Finally, installing
+\`libxft-bgra\` to enable color emoji in suckless software without crashes." 5 70
 yes | sudo -u "$name" $aurhelper -S libxft-bgra-git >/dev/null 2>&1
 
 # Install the dotfiles in the user's home directory
@@ -220,9 +269,15 @@ sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
 killall pulseaudio; sudo -u "$name" pulseaudio --start
 
 # This line, overwriting the `newperms` command above will allow the user to run
-# serveral important commands, `shutdown`, `reboot`, updating, etc. without a password.
+# serveral important commands, `shutdown`, `reboot`, updating, etc. without a
+# password.
 newperms "%wheel ALL=(ALL) ALL #INTERLOPER
-%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay,/usr/bin/pacman -Syyuw --noconfirm"
+%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl
+suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,
+/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin
+/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,
+/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay,/usr/bin/pacman
+-Syyuw --noconfirm"
 
 # Last message! Install complete!
 finalize
